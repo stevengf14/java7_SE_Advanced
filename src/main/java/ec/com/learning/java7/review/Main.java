5aca58d90207f5a2afd4e070fab1dd889a93e507
Steven <stevengf14@gmail.com>
2022-10-04 16:52:39 -0500

    First commit - review java

---
+++ src/main/java/ec/com/learning/java7/review/Main.java
@@ -0,0 +1,16 @@
+package ec.com.learning.java7.review;
+
+/**
+ *
+ * @author Steven Guamán - October 2022
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        for(String param : args){
+            System.out.println("Param "+ param);
+        }
+        
+    }
+    
+}
