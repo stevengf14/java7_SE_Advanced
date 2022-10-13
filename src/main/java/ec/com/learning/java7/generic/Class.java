cd8147c1fff6e33c262493c2a0a97398b6920ee6
Steven <stevengf14@gmail.com>
2022-10-13 18:04:31 -0500

    Generic II

--- src/main/java/ec/com/learning/java7/generic/Class.java
+++ src/main/java/ec/com/learning/java7/generic/Class.java
@@ -4,15 +4,25 @@ package ec.com.learning.java7.generic;
  *
  * @author Steven Guamán - October 2022
  */
-public class Class<T> {
+public class Class<K,T,V,E> {
 
-    private T object;
+    private K objectK;
+    private T objectT;
+    private V objectV;
+    private E objectE;
 
-    public Class(T object) {
-        this.object = object;
+    public Class(K objectK, T objectT, V objectV, E objectE) {
+        this.objectK = objectK;
+        this.objectT = objectT;
+        this.objectV = objectV;
+        this.objectE = objectE;
     }
 
     public void showType() {
-        System.out.println("T is: " + object.getClass().getName());
+        System.out.println("K is: " + objectK.getClass().getName());
+        System.out.println("T is: " + objectT.getClass().getName());
+        System.out.println("V is: " + objectV.getClass().getName());
+        System.out.println("E is: " + objectE.getClass().getName());
     }
+    
 }
--- src/main/java/ec/com/learning/java7/generic/Main.java
+++ src/main/java/ec/com/learning/java7/generic/Main.java
@@ -1,21 +1,45 @@
 package ec.com.learning.java7.generic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Steven Guamán - October 2022
  */
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+
         // Generic I
-        
-        Class<String> c = new Class<>("Steven");
+        /*Class<String> c = new Class<>("Steven");
         c.showType();
         
         Class<Integer> c1 = new Class<>(1);
-        c1.showType();
+        c1.showType();*/
+        // Generic II
+        /*// List list = new ArrayList();
+        List<String> list = new ArrayList();
+        list.add("Steven");
+        //list.add(1);
+        //String text = (String)list.get(0); // casting
         
+        String[] array = new String[2];
+        array[0]="Steven";
+        // array[1]=1;*/
+        Class<String, Integer, String, Double> c = new Class<>("Steven", 123, "Andres", 12.3);
+        c.showType();
+        System.out.println("----------------");
+        
+        List<Class<String, Integer, String, Double>> list = new ArrayList<>();
+        list.add(new Class("Steven", 123, "Andres", 12.3));
+        list.add(new Class("Andres", 345, "Jose", 34.3));
+        list.add(new Class("Anorra", 678, "Esteban", 56.3));
+
+        for (Class<String, Integer, String, Double> cl : list) {
+            cl.showType();
+            System.out.println("***************");
+        }
     }
-    
+
 }
